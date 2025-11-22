9ecd4a5a1c58423c771fb6f3c7c1b1c69e053636
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2025-11-22 11:49:48 -0500

    Update List Functionality
    This commit updates the main.js file so list items can be moved from 'new tasks' to 'tasks in progress'

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
-document.querySelector(`#submit`).addEventListener('click', addListItem);
+document.querySelector('#submit').addEventListener('click', addListItem);
 
-counter = 0;
-myListArray = [];
+let counter = 0;
+let myListArray = [];
 
 //Function to add new list item to array
 function addListItem(e) {
     e.preventDefault();
 
-    itemValue = document.querySelector(`#new-list-item`).value;
-    itemNumber = counter;
+    let itemValue = document.querySelector('#new-list-item').value;
+    let itemNumber = counter;
 
     myListArray.push({itemNumber, itemValue});
 
-    document.querySelector(`#new-list-item`).value = '';    
+    document.querySelector('#new-list-item').value = '';    
 
+    //console check to make sure items are being added to the array correctly
     console.log(myListArray);
 
-    counter++
+    counter++;
 
     updateList();
 }
 
 //Function to display list contents
 function updateList() {
-    const container = document.querySelector(`#listContainer`);
+    const container = document.querySelector('#listContainer');
     container.innerHTML = "";
 
     myListArray.forEach(item => {
@@ -32,6 +33,52 @@ function updateList() {
 
         checkbox.type = "checkbox";
         checkbox.id = `list-item-${item.itemNumber}`;
+        checkbox.classList = "checkbox"
+        checkbox.name = `list-item-${item.itemNumber}`;
+
+        //Adds event listen for when the boxes are checked
+        checkbox.addEventListener('change', tasksInProgress);
+
+        li.appendChild(checkbox);
+        li.append(item.itemValue);
+
+        container.appendChild(li);
+    });
+}
+
+//Function to move tasks from initial list to In Progress List
+let inProgressArray = [];
+
+function tasksInProgress() {
+    if (this.checked) {
+        const itemNumber = parseInt(this.id.replace('list-item-', ''), 10);
+
+        const index = myListArray.findIndex(item => item.itemNumber === itemNumber);
+        if (index!== -1) {
+            const [item] = myListArray.splice(index, 1);
+            inProgressArray.push(item);
+        }
+
+        //console check to make sure items are being added to the array correctly
+        console.log(inProgressArray);
+
+        updateList();
+        updateInProgress();
+    }
+}
+
+//Function to display list items in the In Progress List
+function updateInProgress() {
+    const container = document.querySelector('#inProgressContainer');
+    container.innerHTML = "";
+
+    inProgressArray.forEach(item => {
+        const li = document.createElement("li");
+        const checkbox = document.createElement("input");
+
+        checkbox.type = "checkbox";
+        checkbox.id = `list-item-${item.itemNumber}`;
+        // checkbox.classList = "checkbox"
         checkbox.name = `list-item-${item.itemNumber}`;
 
         li.appendChild(checkbox);
